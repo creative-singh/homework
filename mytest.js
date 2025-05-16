2d90e5c3d933073c5cae646730a765c726edfd0a
bsi8616 <bhsingh@hollandamerica.com>
2025-05-17 02:02:01 +0530

    Testing

--- mytest.js
+++ mytest.js
@@ -1,19 +1,55 @@
-var lengthOfLongestSubstring = function(s) {
-    let l = 0; let r = 0;
-    let max = 0;
-    let len = s.length;
-    let map = new Map()
-    while(r < len) {
-        if(map.get(s[r]) >= l) {
-            l = map.get(s[r]) + 1
-        } 
-        let currMax = r - l + 1
-        if(max < currMax) max = currMax
-        map.set(s[r], r)
-        r++
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
     }
-    // console.log({max, l, r, map})
-    return max
-};
-
-lengthOfLongestSubstring("pwwkew")
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
+    }
+  
+    isEmpty() {
+      return !this.head;
+    }
+  
+    print() {
+      if (this.isEmpty()) return 'List is empty.';
+      let output = [];
+      let ptr = this.head;
+      while (ptr) {
+        output.push(ptr.value);
+        ptr = ptr.next;
+      }
+      return output;
+    }
+  
+    insert(val, pos) {
+      let node = new Node(val);
+      // Insertion in Empty List
+      if (this.isEmpty()) {
+        this.head = node;
+        return;
+      }
+      // Insertion at Head
+      if (pos === 1) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        let ptr = this.head;
+        // Insert at any Position
+        console.log({ pos });
+      }
+    }
+  }
+  
+  const list = new LinkedList();
+  console.log(list.isEmpty());
+  list.insert(10);
+  list.insert(5, 1);
+  list.insert(3, 1);
+  list.insert(20, 2);
+  
+  console.log(list.print());
+  
